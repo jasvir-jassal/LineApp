20b09f7fbdb4ea328d01e1688c16694fa0c8ace5
Jasvir Jassal <jasvir.jassal@outlook.com>
2020-12-10 14:40:52 +0100

    Import modules and componenents in app.module.ts
    Importing all the `Angular` components modules and the `Bootstrap` module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LightSwitchComponent } from './light-switch/light-switch.component';
+import { StatsComponent } from './stats/stats.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LightSwitchComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
